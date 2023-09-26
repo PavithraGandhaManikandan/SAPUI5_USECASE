6d94d2ccbf3c0df81047ea5c78c457a9f6e231ce
PavithraGandhaManikandan <146014593+PavithraGandhaManikandan@users.noreply.github.com>
2023-09-26 23:01:17 +0530

    Create OrdersJourney.js

---
+++ test/integration/ordersList/OrdersJourney.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/test/opaQunit",
+    "sap/ui/test/Opa5"
+    ], function (opaTest, Opa5) {
+    "use strict";
+
+    QUnit.module("Downtime plugin", function () {
+     //Add tests
+
+    });
+});  
